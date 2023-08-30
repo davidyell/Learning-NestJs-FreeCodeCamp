10bf9f824fc015a868e08e339de55f9b5224aad7
David Yell <david.yell@mdgroup.com>
2023-08-30 17:45:21 +0100

    Module updates
    Enable the typeorm datasource to be available for injection
    Export the user service so it's inejctable

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { appDataSourceOptions } from './database/datasource/app.datasource';
+import { UserService } from './user/user.service';
+import { DataSource } from 'typeorm';
 
 @Module({
   imports: [
@@ -15,6 +17,8 @@ import { appDataSourceOptions } from './database/datasource/app.datasource';
     BookmarkModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
