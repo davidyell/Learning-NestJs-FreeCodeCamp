71ff769ebba08ef7bdf996e95659e8461f925ee4
David Yell <david.yell@mdgroup.com>
2023-08-30 15:40:39 +0100

    Fixed the entity and migration pathing so it can connect to the database

--- src/database/datasource/app.datasource.ts
+++ src/database/datasource/app.datasource.ts
@@ -2,13 +2,14 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 
 export const appDataSourceOptions: DataSourceOptions = {
   type: 'mysql',
-  host: '127.0.0.1',
+  host: 'localhost',
   port: 3306,
   username: 'root',
   password: 'root',
   database: 'nestjs-beginner',
   timezone: '+00:00',
-  entities: ['src/**/*.entity.ts'],
-  migrations: ['src/migrations/*-migrations.ts'],
+  entities: ['*.entity.ts'],
+  migrations: ['migrations/*-migrations.ts'],
+  logging: true,
 };
 export const appDataSource = new DataSource(appDataSourceOptions);
