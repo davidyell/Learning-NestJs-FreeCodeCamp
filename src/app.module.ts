06c75b590630f7aa71b9840b4c94c41fb22097df
David Yell <david.yell@mdgroup.com>
2023-09-08 17:30:49 +0100

    Implement the config service
    Swapped the TypeORM module to a factory so the configservice can inject variables from the env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,31 @@ import { BookmarkModule } from './bookmark/bookmark.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { appDataSourceOptions } from './database/datasource/app.datasource';
 import { UserService } from './user/user.service';
-import { DataSource } from 'typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(appDataSourceOptions), AuthModule, UserModule, BookmarkModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        return {
+          ...appDataSourceOptions,
+          ...{
+            host: configService.getOrThrow('DB_HOST'),
+            port: configService.getOrThrow('DB_PORT'),
+            username: configService.getOrThrow('DB_USER'),
+            password: configService.getOrThrow('DB_PWD'),
+          },
+        } as DataSourceOptions;
+      },
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+    ConfigModule.forRoot({}),
+  ],
   controllers: [AppController],
   providers: [AppService, UserService],
 })
